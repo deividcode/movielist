da44232e782f947cb3b30d9cbd3fe3ac273a5c40
Jose David <quinteromda@gmail.com>
2022-03-14 14:25:44 -0400

    Agregando Seccion Populares

--- dist/assets/app.js
+++ dist/assets/app.js
@@ -1,67 +1,67 @@
 const p = function polyfill() {
-  const relList = document.createElement("link").relList;
-  if (relList && relList.supports && relList.supports("modulepreload")) {
-    return;
-  }
-  for (const link of document.querySelectorAll('link[rel="modulepreload"]')) {
-    processPreload(link);
-  }
-  new MutationObserver((mutations) => {
-    for (const mutation of mutations) {
-      if (mutation.type !== "childList") {
-        continue;
-      }
-      for (const node of mutation.addedNodes) {
-        if (node.tagName === "LINK" && node.rel === "modulepreload")
-          processPreload(node);
-      }
+    const relList = document.createElement("link").relList;
+    if (relList && relList.supports && relList.supports("modulepreload")) {
+        return;
     }
-  }).observe(document, { childList: true, subtree: true });
-  function getFetchOpts(script) {
-    const fetchOpts = {};
-    if (script.integrity)
-      fetchOpts.integrity = script.integrity;
-    if (script.referrerpolicy)
-      fetchOpts.referrerPolicy = script.referrerpolicy;
-    if (script.crossorigin === "use-credentials")
-      fetchOpts.credentials = "include";
-    else if (script.crossorigin === "anonymous")
-      fetchOpts.credentials = "omit";
-    else
-      fetchOpts.credentials = "same-origin";
-    return fetchOpts;
-  }
-  function processPreload(link) {
-    if (link.ep)
-      return;
-    link.ep = true;
-    const fetchOpts = getFetchOpts(link);
-    fetch(link.href, fetchOpts);
-  }
+    for (const link of document.querySelectorAll("link[rel=\"modulepreload\"]")) {
+        processPreload(link);
+    }
+    new MutationObserver((mutations) => {
+        for (const mutation of mutations) {
+            if (mutation.type !== "childList") {
+                continue;
+            }
+            for (const node of mutation.addedNodes) {
+                if (node.tagName === "LINK" && node.rel === "modulepreload")
+                    processPreload(node);
+            }
+        }
+    }).observe(document, { childList: true, subtree: true });
+    function getFetchOpts(script) {
+        const fetchOpts = {};
+        if (script.integrity)
+            fetchOpts.integrity = script.integrity;
+        if (script.referrerpolicy)
+            fetchOpts.referrerPolicy = script.referrerpolicy;
+        if (script.crossorigin === "use-credentials")
+            fetchOpts.credentials = "include";
+        else if (script.crossorigin === "anonymous")
+            fetchOpts.credentials = "omit";
+        else
+            fetchOpts.credentials = "same-origin";
+        return fetchOpts;
+    }
+    function processPreload(link) {
+        if (link.ep)
+            return;
+        link.ep = true;
+        const fetchOpts = getFetchOpts(link);
+        fetch(link.href, fetchOpts);
+    }
 };
 p();
 var style = "";
 let resultadosMovie = document.querySelector(".resultados");
 class Ui {
-  constructor() {
-  }
-  limpiarCartelera() {
-    while (resultadosMovie.firstElementChild) {
-      resultadosMovie.removeChild(resultadosMovie.firstElementChild);
+    constructor() {
     }
-  }
-  mostrarCartelera(datos, ubicacion) {
-    console.log(datos);
-    let datosRevisados = datos.filter((e) => e.poster_path != null);
-    if (ubicacion == "inicio") {
-      datosRevisados = datosRevisados.slice(0, 10);
-      console.log(datosRevisados);
-      let catalogoPopulares = document.querySelector(".b-popular__catalogo-pelicula");
-      console.log("Hola Aqui");
-      datosRevisados.forEach((movie) => {
-        let { title, id, poster_path, release_date } = movie;
-        let imgMovie = "https://image.tmdb.org/t/p/w500/" + poster_path;
-        catalogoPopulares.insertAdjacentHTML("afterbegin", `
+    limpiarCartelera() {
+        while (resultadosMovie.firstElementChild) {
+            resultadosMovie.removeChild(resultadosMovie.firstElementChild);
+        }
+    }
+    mostrarCartelera(datos, ubicacion) {
+        console.log(datos);
+        let datosRevisados = datos.filter((e) => e.poster_path != null);
+        if (ubicacion == "inicio") {
+            datosRevisados = datosRevisados.slice(0, 10);
+            console.log(datosRevisados);
+            let catalogoPopulares = document.querySelector(".b-popular__catalogo-pelicula");
+            console.log("Hola Aqui");
+            datosRevisados.forEach((movie) => {
+                let { title, id, poster_path, release_date } = movie;
+                let imgMovie = "https://image.tmdb.org/t/p/w500/" + poster_path;
+                catalogoPopulares.insertAdjacentHTML("afterbegin", `
             
                     <div class="movie-catalog">     
 
@@ -84,13 +84,13 @@ class Ui {
                     </div>                        
             
                 `);
-      });
-    }
-    if (ubicacion == "cartelera") {
-      datosRevisados.forEach((movie) => {
-        let { title, id, poster_path, release_date } = movie;
-        let imgMovie = "https://image.tmdb.org/t/p/w500/" + poster_path;
-        resultadosMovie.insertAdjacentHTML("afterbegin", `
+            });
+        }
+        if (ubicacion == "cartelera") {
+            datosRevisados.forEach((movie) => {
+                let { title, id, poster_path, release_date } = movie;
+                let imgMovie = "https://image.tmdb.org/t/p/w500/" + poster_path;
+                resultadosMovie.insertAdjacentHTML("afterbegin", `
                 
                     <div class="box-pelicula">            
                         <a class="box-pelicula__enlace" href="pelicula.html?id=${id}">                
@@ -106,49 +106,29 @@ class Ui {
                     </div>                        
                 
                 `);
-      });
+            });
+        }
     }
-  }
 }
 const ui = new Ui();
 const BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = "api_key=bc1ec0aeff28eba96fa4dc1b0c704b97";
 const API_URL_CARTELERA = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
 function pedirPopulares(ubicacion) {
-  fetch(API_URL_CARTELERA).then((response) => response.json()).then((movies) => ui.mostrarCartelera(movies.results, ubicacion));
-}
-function peticionAPI(titulo, seriePelicula, contenidoAdulto, date) {
-  let API_URL = BASE_URL + `/search/${seriePelicula}?` + API_KEY + `&language=en-US&query=${titulo}&include_adult=${contenidoAdulto}&year=${date}`;
-  console.log(API_URL);
-  fetch(API_URL).then((response) => response.json()).then((movies) => {
-    ui.limpiarCartelera();
-    ui.mostrarCartelera(movies.results);
-  });
+    fetch(API_URL_CARTELERA).then((response) => response.json()).then((movies) => ui.mostrarCartelera(movies.results, ubicacion));
 }
 document.querySelector(".box-pelicula");
-let pageCartelera = document.querySelector(".page-cartelera");
-let formulario = document.querySelector("#formulario");
-let formularioInputSearch = document.querySelector(".formulario__inputSearch");
+document.querySelector(".page-cartelera");
+document.querySelector("#formulario");
+document.querySelector(".formulario__inputSearch");
 document.querySelector(".formulario__submit");
-let formularioInputSP = document.querySelector("input[name='seriePelicula']");
-let formularioInputAC = document.querySelector("input[name='contenidoAdulto']");
+document.querySelector("input[name='seriePelicula']");
+document.querySelector("input[name='contenidoAdulto']");
 document.addEventListener("DOMContentLoaded", function() {
-  if (location.href == "http://localhost:8080/") {
-    pedirPopulares("inicio");
-  }
-  if (location.href == "http://localhost:8080/cartelera.html") {
-    pedirPopulares("cartelera");
-  }
-  if (pageCartelera.hidden == false)
-    pedirPopulares();
-  formulario.addEventListener("submit", tomarDatosFormulario);
+    if (location.href == "http://localhost:4173/") {
+        pedirPopulares("inicio");
+    }
+    if (location.href == "http://localhost:4173/cartelera.html") {
+        pedirPopulares("cartelera");
+    }
 });
-function tomarDatosFormulario(e) {
-  var _a;
-  e.preventDefault();
-  let titulo = (_a = formularioInputSearch.value) != null ? _a : null;
-  let seriePelicula = formularioInputSP.value == "pelicula" ? "movie" : "serie";
-  let contenidoAdulto = formularioInputAC.checked ? true : false;
-  console.log(titulo, seriePelicula, contenidoAdulto);
-  peticionAPI(titulo, seriePelicula, contenidoAdulto);
-}
