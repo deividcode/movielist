d5970839cb7b313b9d38095d951b30d2417938ee
Jose David <quinteromda@gmail.com>
2022-04-07 15:36:34 -0400

    Ordenar Peliculas, Mejorando el estilo

--- dist/assets/app.js
+++ dist/assets/app.js
@@ -1,3 +1,9 @@
+var __defProp = Object.defineProperty;
+var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
+var __publicField = (obj, key, value) => {
+  __defNormalProp(obj, typeof key !== "symbol" ? key + "" : key, value);
+  return value;
+};
 const p = function polyfill() {
   const relList = document.createElement("link").relList;
   if (relList && relList.supports && relList.supports("modulepreload")) {
@@ -41,24 +47,59 @@ const p = function polyfill() {
 };
 p();
 var style = "";
-let resultadosMovie = document.querySelector(".resultados");
+let resultadosMovie = document.querySelector(".cartelera__resultado");
 class Ui {
   constructor() {
+    __publicField(this, "arrPeliculas", []);
   }
   limpiarCartelera() {
     while (resultadosMovie.firstElementChild) {
       resultadosMovie.removeChild(resultadosMovie.firstElementChild);
     }
   }
-  mostrarCartelera(datos, ubicacion) {
-    console.log(datos);
-    let datosRevisados = datos.filter((e) => e.poster_path != null);
+  ordenarPelicula() {
+    let selectValueOrden2 = document.querySelector(".caja-busqueda__opciones select");
+    console.log(this.arrPeliculas);
+    switch (selectValueOrden2.value) {
+      case "a-up":
+        return this.arrPeliculas.sort((a, b) => {
+          let { title } = a;
+          let titleM = title == null ? "name" : "title";
+          return b[titleM] < a[titleM] ? -1 : 1;
+        });
+      case "a-down":
+        return this.arrPeliculas.sort((a, b) => {
+          let { title } = a;
+          let titleM = title == null ? "name" : "title";
+          return b[titleM] > a[titleM] ? -1 : 1;
+        });
+      case "f-up":
+        return this.arrPeliculas.sort((a, b) => {
+          let { first_air_date } = a;
+          let dateM = first_air_date == null ? "release_date" : "first_air_date";
+          return new Date(a[dateM]) - new Date(b[dateM]);
+        });
+      case "f-down":
+        return this.arrPeliculas.sort((a, b) => {
+          let { first_air_date } = a;
+          let dateM = first_air_date == null ? "release_date" : "first_air_date";
+          return new Date(b[dateM]) - new Date(a[dateM]);
+        });
+      default:
+        return this.arrPeliculas;
+    }
+  }
+  mostrarPeliculas(datos, ubicacion) {
+    this.limpiarCartelera();
+    this.arrPeliculas = datos != null ? datos : this.arrPeliculas;
+    console.log(this.arrPeliculas);
+    this.arrPeliculas = this.ordenarPelicula();
     if (ubicacion == "inicio") {
-      datosRevisados = datosRevisados.slice(0, 10);
-      console.log(datosRevisados);
+      this.arrPeliculas = this.arrPeliculas.slice(0, 10);
+      console.log(this.arrPeliculas);
       let catalogoPopulares = document.querySelector(".b-popular__catalogo-pelicula");
       console.log("Hola Aqui");
-      datosRevisados.forEach((movie) => {
+      this.arrPeliculas.forEach((movie) => {
         let { title, id, poster_path, release_date, vote_average } = movie;
         let imgMovie = "https://image.tmdb.org/t/p/w500/" + poster_path;
         catalogoPopulares.insertAdjacentHTML("afterbegin", `
@@ -88,48 +129,88 @@ class Ui {
       });
     }
     if (ubicacion == "cartelera") {
-      datosRevisados.forEach((movie) => {
-        let { title, id, poster_path, release_date } = movie;
+      this.arrPeliculas.forEach((movie) => {
+        let { title, name, id, poster_path, release_date, first_air_date, vote_average } = movie;
         let imgMovie = "https://image.tmdb.org/t/p/w500/" + poster_path;
         resultadosMovie.insertAdjacentHTML("afterbegin", `
-                
-                    <div class="box-pelicula">            
-                        <a class="box-pelicula__enlace" href="pelicula.html?id=${id}">                
-                            <div class="box-pelicula__img">
-                                <img src="${imgMovie}">                    
+            
+                    <div class="movie-catalog">     
+
+                        <div class="movie-catalog__add-list">
+                            +
+                        </div>       
+
+                        <a class="movie-catalog__link" href="pelicula.html?id=${id}">                
+                        
+                            <div class="movie-catalog__image">
+                                <img src="${imgMovie}">  
+                                <div class="movie-catalog__rating">${vote_average}</div>                  
                             </div>
-                        </a>
         
-                        <div class="box-pelicula__text">                                
-                            <h5>${title}</h5>
-                            <p class="box-pelicula__date">${release_date}</p>                        
+                            <div class="movie-catalog__text">                                
+                                <h5 class="movie-catalog__title">${title != null ? title : name}</h5>
+                                <p class="movie-catalog__date">${release_date != null ? release_date : first_air_date}</p>                        
                             </div>
+
+                        </a>
                     </div>                        
-                
+            
                 `);
       });
     }
+  }
+  mostrarError(msj, ubicacion) {
+    if (ubicacion == "caja busqueda") {
+      let msjError = document.querySelector(".mensaje-error");
+      msjError.style.display = "block";
+    }
+    if (ubicacion == "cartelera") {
+      console.log("Error en la ccartelera");
+    }
+    console.log(msj);
   }
 }
 const ui = new Ui();
 const BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = "api_key=bc1ec0aeff28eba96fa4dc1b0c704b97";
-const API_URL_CARTELERA = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
+const API_URL_POPULARES = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
 function pedirPopulares(ubicacion) {
-  fetch(API_URL_CARTELERA).then((response) => response.json()).then((movies) => ui.mostrarCartelera(movies.results, ubicacion));
+  fetch(API_URL_POPULARES).then((response) => response.json()).then((response) => response.results.filter((e) => e.poster_path != null)).then((movies) => ui.mostrarPeliculas(movies, ubicacion));
 }
-document.querySelector(".box-pelicula");
-document.querySelector(".page-cartelera");
-document.querySelector("#formulario");
-document.querySelector(".formulario__inputSearch");
-document.querySelector(".formulario__submit");
-document.querySelector("input[name='seriePelicula']");
-document.querySelector("input[name='contenidoAdulto']");
+function pedirPeliculas(titulo, seriePelicula) {
+  let API_URL = BASE_URL + `/search/${seriePelicula}?` + API_KEY + `&language=en-US&query=${titulo}`;
+  fetch(API_URL).then((response) => response.json()).then((response) => response.results.filter((e) => e.poster_path != null)).then((movies) => {
+    ui.mostrarPeliculas(movies, "cartelera");
+  });
+}
+let formulario = document.querySelector("#caja-busqueda__formulario");
+let formularioInputSearch = document.querySelector(".caja-busqueda__inputSearch");
+document.querySelector(".caja-busqueda__submit");
+let selectValueOrden = document.querySelector(".caja-busqueda__opciones select");
 document.addEventListener("DOMContentLoaded", function() {
-  if (location.href == "https://movielistd.netlify.app/") {
+  if (location.href == "http://localhost:8080/" || location.href == "http://localhost:8080/#") {
     pedirPopulares("inicio");
   }
-  if (location.href == "http://localhost:8080/cartelera.html") {
+  if (location.href == "http://localhost:8080/cartelera.html" || location.href == "http://localhost:8080/cartelera.html/#") {
     pedirPopulares("cartelera");
   }
+  formulario.addEventListener("submit", tomarDatosFormulario);
+  selectValueOrden.addEventListener("change", function saludo() {
+    console.log("CAMbio");
+    ui.mostrarPeliculas(null, "cartelera");
+  });
 });
+function tomarDatosFormulario(e) {
+  var _a;
+  e.preventDefault();
+  let formularioInputSP = document.querySelector("input[name='seriePelicula']:checked");
+  if (formularioInputSearch.value == "") {
+    return ui.mostrarError("", "caja busqueda");
+  } else {
+    let msjError = document.querySelector(".mensaje-error");
+    msjError.style.display = "none";
+  }
+  let titulo = (_a = formularioInputSearch.value) != null ? _a : null;
+  let seriePelicula = formularioInputSP.value == "pelicula" ? "movie" : "tv";
+  pedirPeliculas(titulo, seriePelicula);
+}
